03c00b13bc8dbb38759e09ca1966d7727251eed1
Sebastian <fonsecasebas@hotmail.es>
2024-06-13 12:30:20 -0600

    Primera Clase

---
+++ parte_superior.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Proyecto de tercera I" content="Página de prueba">
+    <title>
+        Tercera 1
+    </title>
+    <link rel="stylesheet" href="./Styles/main.css">
+
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <a href="./index.php"> <img src="./images/udlg.png" alt="">
+    </a>
+       
+        <p class="texto-menu-izquierda">
+            U de las Gradas
+        </p>
+
+    </div>
+</menu>
