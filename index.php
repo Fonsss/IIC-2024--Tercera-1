d749feb257ae4cc8bf63e6f2a54c16ffc49e4c4c
Sebastian <fonsecasebas@hotmail.es>
2024-06-19 14:35:39 -0600

    Semana 2

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php require_once "./parte_superior.php"        ?>
 
+<div class="titulo">
+<p>Estudiar una carrera universitaria es un privilegio de pocos, ¿qué estás esperando?</p>
+</div>
 
+<body>
+    <div class="main-img">
+        <img src="./images/Clases-en-linea-1.png" alt="">
 
-
+    </div>
+</body>
 
 
 <?php require_once "./parte_inferior.php"        ?>
--- parte_superior.php
+++ parte_superior.php
@@ -4,21 +4,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="Proyecto de tercera I" content="Página de prueba">
-    <title>
-        Tercera 1
-    </title>
     <link rel="stylesheet" href="./Styles/main.css">
 
 </head>
 
 <menu>
     <div class="menu-izquierda">
-        <a href="./index.php"> <img src="./images/udlg.png" alt="">
+        <a href="./index.php"> 
+            <img src="./images/logo1.jpeg" alt="">
     </a>
-       
-        <p class="texto-menu-izquierda">
-            U de las Gradas
-        </p>
-
+    </div>
+    <div class="menu-derecha">
+        <a class="enlaces-menu-derecha"href="https://google.com">Primer Ingreso</a>
+        <a class="enlaces-menu-derecha"href="https://google.com">Aula en línea</a>
     </div>
 </menu>
